5c5c6bbfc5b7f49edefa04c7494e155d4b1a5316
tfrawlz <t.c.frawley1012@gmail.com>
2020-10-30 20:14:58 -0500

    Renamed usecase files and usecase methods. Added test files for the usecase files added

--- cocktailapp/lib/features/cocktail_info/domain/repositories/cocktail_info_repository.dart
+++ cocktailapp/lib/features/cocktail_info/domain/repositories/cocktail_info_repository.dart
@@ -5,15 +5,15 @@ import 'package:cocktailapp/features/cocktail_info/domain/entities/ingredient_in
 import 'package:dartz/dartz.dart';
 
 abstract class CocktailInfoRepository {
-  Future<Either<Failure, DrinkInfoList>> getSearchByCocktailResults(String searchTerm);
-  Future<Either<Failure, DrinkInfoList>> getAllCocktailsStartingWithTheLetter(String letter);
-  Future<Either<Failure, IngredientInfo>> getIngredientInfo(String ingredient);
+  Future<Either<Failure, CategoriesList>> getCategories();
   Future<Either<Failure, DrinkInfo>> getCocktailInfoByName(String drink);
+  Future<Either<Failure, DrinkInfoList>> getCocktailsByFirstLetter(String letter);
+  Future<Either<Failure, DrinkInfoList>> getCocktailsBySearchResults(String searchTerm);
+  Future<Either<Failure, PartialDrinkInfoList>> getCocktailsFilteredByCategory(String category);
+  Future<Either<Failure, PartialDrinkInfoList>> getCocktailsFilteredByGlass(String glass);
+  Future<Either<Failure, PartialDrinkInfoList>> getCocktailsFilteredByIngredient(String ingredient);
+  Future<Either<Failure, GlassesList>> getGlasses();
+  Future<Either<Failure, IngredientInfo>> getIngredientInfo(String ingredient);
+  Future<Either<Failure, IngredientInfoList>> getIngredients();
   Future<Either<Failure, DrinkInfo>> getRandomCocktailInfo();
-  Future<Either<Failure, PartialDrinkInfoList>> getDrinkListFilteredByIngredient(String ingredient);
-  Future<Either<Failure, PartialDrinkInfoList>> getDrinkListFilteredByCategory(String category);
-  Future<Either<Failure, PartialDrinkInfoList>> getDrinkListFilteredByGlass(String glass);
-  Future<Either<Failure, CategoriesList>> getListOfCategories();
-  Future<Either<Failure, GlassesList>> getListOfGlasses();
-  Future<Either<Failure, IngredientInfoList>> getListOfIngredients();
 }
