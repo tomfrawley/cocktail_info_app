526caee9310b0d1e920403970e661bb55b970290
tfrawlz <t.c.frawley1012@gmail.com>
2020-11-04 16:55:46 -0600

    wrote fromJson deserialization factories for all the CocktailInfoTemplate based models

--- cocktailapp/lib/features/cocktail_info/data/models/cocktail_info_model.dart
+++ cocktailapp/lib/features/cocktail_info/data/models/cocktail_info_model.dart
@@ -18,6 +18,36 @@ class CocktailInfoModel extends CocktailInfo {
             glass: glass,
             instructions: instructions,
             ingredientsAndMeasurements: ingredientsAndMeasurements);
+
+  factory CocktailInfoModel.fromJson(Map<String, dynamic> json) {
+    Map<String, String> ingredientsAndMeasurementsMap = {
+      json['strIngredient1']: json['strMeasure1'],
+      json['strIngredient2']: json['strMeasure2'],
+      json['strIngredient3']: json['strMeasure3'],
+      json['strIngredient4']: json['strMeasure4'],
+      json['strIngredient5']: json['strMeasure5'],
+      json['strIngredient6']: json['strMeasure6'],
+      json['strIngredient7']: json['strMeasure7'],
+      json['strIngredient8']: json['strMeasure8'],
+      json['strIngredient9']: json['strMeasure9'],
+      json['strIngredient10']: json['strMeasure10'],
+      json['strIngredient11']: json['strMeasure11'],
+      json['strIngredient12']: json['strMeasure12'],
+      json['strIngredient13']: json['strMeasure13'],
+      json['strIngredient14']: json['strMeasure14'],
+      json['strIngredient15']: json['strMeasure15'],
+    };
+
+    return CocktailInfoModel(
+      id: int.parse(json['idDrink']),
+      name: json['strDrink'],
+      thumbnail: json['strDrinkThumb'],
+      category: json['strCategory'],
+      glass: json['strGlass'],
+      instructions: json['strInstructions'],
+      ingredientsAndMeasurements: ingredientsAndMeasurementsMap,
+    );
+  }
 }
 
 class PartialCocktailInfoModel extends PartialCocktailInfo {
@@ -26,22 +56,42 @@ class PartialCocktailInfoModel extends PartialCocktailInfo {
     String name,
     String thumbnail,
   }) : super(id: id, name: name, thumbnail: thumbnail);
+
+  factory PartialCocktailInfoModel.fromJson(Map<String, dynamic> json) {
+    return PartialCocktailInfoModel(
+      id: int.parse(json['idDrink']),
+      name: json['strDrink'],
+      thumbnail: json['strDrinkThumb'],
+    );
+  }
 }
 
 class GlassModel extends Glass {
   GlassModel({
     String glass,
   }) : super(glass: glass);
+
+  factory GlassModel.fromJson(Map<String, dynamic> json) {
+    return GlassModel(glass: json['strGlass']);
+  }
 }
 
 class CategoryModel extends Category {
   CategoryModel({
     String category,
   }) : super(category: category);
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(category: json['strCategory']);
+  }
 }
 
 class IngredientModel extends Ingredient {
   IngredientModel({
     String ingredient,
   }) : super(ingredient: ingredient);
+
+  factory IngredientModel.fromJson(Map<String, dynamic> json) {
+    return IngredientModel(ingredient: json['strIngredient1']);
+  }
 }
--- cocktailapp/test/features/cocktail_info/data/models/cocktail_info_model_test.dart
+++ cocktailapp/test/features/cocktail_info/data/models/cocktail_info_model_test.dart
@@ -1,25 +1,45 @@
+import 'dart:convert';
+
 import 'package:cocktailapp/features/cocktail_info/data/models/cocktail_info_model.dart';
 import 'package:cocktailapp/features/cocktail_info/domain/entities/cocktail_info.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
 void main() {
   final tCocktailInfoModel = CocktailInfoModel(
     id: 1,
-    name: 'test',
-    thumbnail: 'test',
-    category: 'test',
-    glass: 'test',
-    instructions: 'test',
-    ingredientsAndMeasurements: {'test': 'test'},
+    name: 'Test',
+    thumbnail: 'Test',
+    category: 'Test',
+    glass: 'Test',
+    instructions: 'Test',
+    ingredientsAndMeasurements: {
+      'Test': 'Test',
+      'Test2': 'Test2',
+      'Test3': 'Test3',
+      'Test4': 'Test4',
+      'Test5': 'Test5',
+      'Test6': 'Test6',
+      'Test7': 'Test7',
+      'Test8': 'Test8',
+      'Test9': 'Test9',
+      'Test10': 'Test10',
+      'Test11': 'Test11',
+      'Test12': 'Test12',
+      'Test13': 'Test13',
+      'Test14': 'Test14',
+      'Test15': 'Test15',
+    },
   );
   final tPartialCocktailInfoModel = PartialCocktailInfoModel(
     id: 1,
-    name: 'test',
-    thumbnail: 'test',
+    name: 'Test',
+    thumbnail: 'Test',
   );
-  final tGlass = GlassModel(glass: 'glass');
-  final tCategory = CategoryModel(category: 'category');
-  final tIngredient = IngredientModel(ingredient: 'ingredient');
+  final tGlassModel = GlassModel(glass: 'Test Glass');
+  final tCategoryModel = CategoryModel(category: 'Test Category');
+  final tIngredientModel = IngredientModel(ingredient: 'Test Ingredient');
 
   group('CocktailInfoTemplate based models', () {
     test('should be a subclass of CocktailInfo entity', () async {
@@ -34,17 +54,64 @@ void main() {
 
     test('should be a subclass of Glass entity', () async {
       //Assert
-      expect(tGlass, isA<Glass>());
+      expect(tGlassModel, isA<Glass>());
     });
 
     test('should be a subclass of Category entity', () async {
       //Assert
-      expect(tCategory, isA<Category>());
+      expect(tCategoryModel, isA<Category>());
     });
 
     test('should be a subclass of Ingredient entity', () async {
       //Assert
-      expect(tIngredient, isA<Ingredient>());
+      expect(tIngredientModel, isA<Ingredient>());
+    });
+  });
+
+  group('fromJson', () {
+    test('should return a valid model from the JSON data for a CocktialInfoModel response', () async {
+      //Arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('full_drink_info.json'));
+      //Act
+      final result = CocktailInfoModel.fromJson(jsonMap);
+      //Assert
+      expect(result, tCocktailInfoModel);
+    });
+
+    test('should return a valid model from the JSON data for a PartialCocktailInfoModel response', () async {
+      //Arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('partial_drink.json'));
+      //Act
+      final result = PartialCocktailInfoModel.fromJson(jsonMap);
+      //Assert
+      expect(result, tPartialCocktailInfoModel);
+    });
+
+    test('should return a valid model from the JSON data for a GlassModel response', () async {
+      //Arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('glass.json'));
+      //Act
+      final result = GlassModel.fromJson(jsonMap);
+      //Assert
+      expect(result, tGlassModel);
+    });
+
+    test('should return a valid model from the JSON data for a CategoryModel response', () async {
+      //Arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('category.json'));
+      //Act
+      final result = CategoryModel.fromJson(jsonMap);
+      //Assert
+      expect(result, tCategoryModel);
+    });
+
+    test('should return a valid model from the JSON data for a IngredientModel response', () async {
+      //Arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('ingredient.json'));
+      //Act
+      final result = IngredientModel.fromJson(jsonMap);
+      //Assert
+      expect(result, tIngredientModel);
     });
   });
 }
