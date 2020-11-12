8664030003a9dccb9c132b964101634ea65f133c
tfrawlz <t.c.frawley1012@gmail.com>
2020-11-11 19:25:37 -0600

    refactored repository implementation methods because they all used variations on the same method to call the backend

--- cocktailapp/lib/features/cocktail_info/data/repositories/cocktail_info_repository_impl.dart
+++ cocktailapp/lib/features/cocktail_info/data/repositories/cocktail_info_repository_impl.dart
@@ -14,146 +14,92 @@ class CocktailInfoRepositoryImpl implements CocktailInfoRepository {
 
   CocktailInfoRepositoryImpl({this.remoteDataSource, this.networkInfo});
 
-  @override
-  Future<Either<Failure, Categories>> getCategories() async {
+  dynamic accessCocktailAPI(Function call) async {
     if (await networkInfo.isConnected) {
       try {
-        return Right(await remoteDataSource.getCategories());
+        return Right(await call);
       } on ServerException {
         return Left(ServerFailure());
       }
     } else {
       return Left(NetworkFailure());
     }
+  }
+
+  @override
+  Future<Either<Failure, Categories>> getCategories() async {
+    return accessCocktailAPI(() {
+      remoteDataSource.getCategories();
+    });
   }
 
   @override
   Future<Either<Failure, CocktailInfo>> getCocktailInfoById(int cocktailId) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailInfoById(cocktailId));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailInfoById(cocktailId);
+    });
   }
 
   @override
   Future<Either<Failure, CocktailInfoList>> getCocktailsByFirstLetter(String letter) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailsByFirstLetter(letter));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailsByFirstLetter(letter);
+    });
   }
 
   @override
   Future<Either<Failure, CocktailInfoList>> getCocktailsBySearchResults(String searchTerm) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailsBySearchResults(searchTerm));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailsBySearchResults(searchTerm);
+    });
   }
 
   @override
   Future<Either<Failure, PartialCocktailInfoList>> getCocktailsFilteredByCategory(String category) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailsFilteredByCategory(category));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailsFilteredByCategory(category);
+    });
   }
 
   @override
   Future<Either<Failure, PartialCocktailInfoList>> getCocktailsFilteredByGlass(String glass) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailsFilteredByGlass(glass));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailsFilteredByGlass(glass);
+    });
   }
 
   @override
   Future<Either<Failure, PartialCocktailInfoList>> getCocktailsFilteredByIngredient(String ingredient) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getCocktailsFilteredByIngredient(ingredient));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getCocktailsFilteredByIngredient(ingredient);
+    });
   }
 
   @override
   Future<Either<Failure, Glasses>> getGlasses() async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getGlasses());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getGlasses();
+    });
   }
 
   @override
   Future<Either<Failure, IngredientInfo>> getIngredientInfoById(int ingredientId) async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getIngredientInfoById(ingredientId));
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getIngredientInfoById(ingredientId);
+    });
   }
 
   @override
   Future<Either<Failure, Ingredients>> getIngredients() async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getIngredients());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getIngredients();
+    });
   }
 
   @override
   Future<Either<Failure, CocktailInfo>> getRandomCocktailInfo() async {
-    if (await networkInfo.isConnected) {
-      try {
-        return Right(await remoteDataSource.getRandomCocktialInfo());
-      } on ServerException {
-        return Left(ServerFailure());
-      }
-    } else {
-      return Left(NetworkFailure());
-    }
+    return accessCocktailAPI(() {
+      remoteDataSource.getRandomCocktialInfo();
+    });
   }
 }
